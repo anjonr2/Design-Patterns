6925c8a30ae9a4418411d15fd95b6d559e6dd66d
Anjon Roy <anjon782446@gmail.com>
2025-04-20 22:45:51 +0530

    singleton pattern impl

---
+++ src/com/designpatterns/creational/Singleton/SingletonImpl/Application.java
@@ -0,0 +1,8 @@
+package com.designpatterns.creational.Singleton.SingletonImpl;
+
+public class Application {
+    public static void main(String []args){
+        Logger logger = Logger.getLogger();
+        logger.log("Singleton class");
+    }
+}
